ea50829d86f3b7af74b9c4190d81c47701957af0
Maria Francielly <fransantana143@gmail.com>
2024-04-04 00:09:38 -0300

    Updating the buttons

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -9,7 +9,7 @@ function About(props) {
             <div className='about-text'>
                 <h2> {props.title} </h2>
                 <p> The API was created with the aim of promoting health and well-being in a practical and effective way. Inspired by the growing need for balance in an increasingly fast-paced world, this API offers practical tips, relaxation activities and positive messages to make it easier to incorporate healthy habits into your everyday life. </p>
-                <button> {props.button} </button>
+                <a href='https://github.com/frananisio/websiteHealthAPI' className='cv-btn'>Repository</a>
             </div>
         </div>
     )
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -8,7 +8,7 @@ function Header() {
             <div className='name'>
                 <h1><span>HealthAPI</span><br></br>An API That Helps With Mental And Fisical Health</h1>
                 <p className='details'>A platform developed to promote health and well-being. With practical tips, relaxation activities and positive messages, its goal is to make it easier to incorporate healthy habits into your daily life.</p>
-                <a href='#download' className='cv-btn'>Repository</a>
+                <a href='https://github.com/frananisio/websiteHealthAPI' className='cv-btn'>Repository</a>
             </div>
         </div>
     )
